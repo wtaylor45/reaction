4cc257b355ba08000ce8bf8e7d03c4c35a02cd4a
Will Taylor <taylorw@udel.edu>
2019-03-02 17:02:00 -0500

    Added static waveform

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
 import React from 'react';
 import styled from 'styled-components';
+import ReactWaves from '@dschoon/react-waves';
 import DrumPad from './components/DrumPad';
 import kick from './sounds/Clicky Kick.wav';
 
+const Container = styled.div`
+  height: 100vh;
+`;
+
 const Machine = styled.main`
   display: grid;
-  grid-template-columns: repeat(4, 20vmin);
-  grid-template-rows: repeat(4, 20vmin);
+  grid-template-columns: repeat(4, 10vmin);
+  grid-template-rows: auto repeat(4, 10vmin);
   place-content: center;
   grid-column-gap: 1vmin;
   grid-row-gap: 1vmin;
-  height: 100vh;
-  background-color: #444;
+  background: #444;
+  height: 100%;
+`;
+
+const Wave = styled(ReactWaves)`
+  grid-column: span 4;
+  margin: none;
 `;
 
 const App = () => (
-  <div>
+  <Container>
     <Machine>
+      <Wave
+        audioFile={kick}
+        options={{
+          barHeight: 2,
+          cursorWidth: 0,
+          height: 200,
+          hideScrollbar: true,
+          progressColor: '#EC407A',
+          responsive: true,
+          waveColor: 'dodgerblue'
+        }}
+      />
       <DrumPad />
       <DrumPad />
       <DrumPad />
@@ -34,7 +56,7 @@ const App = () => (
       <DrumPad />
       <DrumPad />
     </Machine>
-  </div>
+  </Container>
 );
 
 export default App;
