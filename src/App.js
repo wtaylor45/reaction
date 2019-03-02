0ab858a48f4bde2366cf5dffcc44720a1549f4f3
Will Taylor <taylorw@udel.edu>
2019-03-02 15:04:34 -0500

    Rough drum pad layout created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
+import DrumPad from './components/DrumPad';
 
-const App = () => <div>Hello World!</div>;
+const Machine = styled.main`
+  display: grid;
+  grid-template-columns: repeat(4, 20vmin);
+  grid-template-rows: repeat(4, 20vmin);
+  place-content: center;
+  grid-column-gap: 1vmin;
+  grid-row-gap: 1vmin;
+  height: 100vh;
+`;
+
+const App = () => (
+  <div>
+    <Machine>
+      <DrumPad />
+      <DrumPad />
+      <DrumPad />
+      <DrumPad />
+      <DrumPad />
+      <DrumPad />
+      <DrumPad />
+      <DrumPad />
+      <DrumPad />
+      <DrumPad />
+      <DrumPad />
+      <DrumPad />
+      <DrumPad />
+      <DrumPad />
+      <DrumPad />
+      <DrumPad />
+    </Machine>
+  </div>
+);
 
 export default App;
---
+++ src/components/DrumPad.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Sound from 'react-sound';
+import kick from '../sounds/Clicky Kick.wav';
+
+const Button = styled.button`
+  background: ${props => (props.playing ? `dodgerblue` : `lightgray`)};
+  border-right: 3px inset gray;
+  border-bottom: 3px inset gray;
+`;
+
+const DrumPad = ({ name }) => {
+  const [playStatus, setPlayStatus] = useState(Sound.status.STOPPED);
+  const [position, setPosition] = useState(0);
+
+  const handleButtonClick = () => {
+    setPosition(0);
+    setPlayStatus(Sound.status.PLAYING);
+  };
+
+  const handleSoundCompletion = () => {
+    setPlayStatus(Sound.status.STOPPED);
+  };
+
+  const handlePlaying = ({ curPos }) => {
+    setPosition(curPos);
+  };
+
+  return (
+    <Button
+      type="button"
+      onClick={handleButtonClick}
+      playing={playStatus === Sound.status.PLAYING}
+    >
+      {name}
+      <Sound
+        url={kick}
+        position={position}
+        onFinishedPlaying={handleSoundCompletion}
+        playStatus={playStatus}
+        onPlaying={handlePlaying}
+      />
+    </Button>
+  );
+};
+
+export default DrumPad;
